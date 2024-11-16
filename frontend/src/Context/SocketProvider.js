6f40cc1a5e0452c0e0f12ef0e097f1a909e7997c
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-11-16 17:06:43 +0530

    WebRTC server

--- frontend/src/Context/SocketProvider.js
+++ frontend/src/Context/SocketProvider.js
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("http://localhost:2001"), []);
+  const socket = useMemo(() => io("https://videocallwebrtc.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
