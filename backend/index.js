38b68428f2b16ffb6032bc483a980a6c77318ab5
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-11-16 17:01:01 +0530

    Backend-issue with cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const { Server } = require("socket.io");
 const io = new Server(2001, {
-    cors: true,
+  cors: {
+    origin: 'https://meet-space-ten.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
 });
 const connect = require('./connection/db');
 const cors = require('cors');
@@ -15,11 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connect();
 const corsOptions = {
-    origin: 'https://meet-space-ten.vercel.app',
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
-    allowedHeaders: 'Content-Type,Authorization'
+  origin: 'https://meet-space-ten.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use('/', Authenticate);
@@ -30,45 +35,45 @@ const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
-    console.log(`Socket Connected`, socket.id);
-    socket.on("room:join", (data) => {
-        const { email, roomID } = data;
-        console.log("Email",email,"RoomID",roomID)
-        emailToSocketIdMap.set(email, socket.id);
-        socketidToEmailMap.set(socket.id, email);
-        io.to(roomID).emit("user:joined", { email, id: socket.id });
-        socket.join(roomID);
-        io.to(socket.id).emit("room:join", data);
-    });
-    socket.on("user:call", ({ to, offer }) => {
-        io.to(to).emit("incomming:call", { from: socket.id, offer });
-      });
-    
-      socket.on("call:accepted", ({ to, ans }) => {
-        io.to(to).emit("call:accepted", { from: socket.id, ans });
-      });
-    
-      socket.on("peer:nego:needed", ({ to, offer }) => {
-        console.log("peer:nego:needed", offer);
-        io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-      });
-    
-      socket.on("peer:nego:done", ({ to, ans }) => {
-        console.log("peer:nego:done", ans);
-        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-      });
-      socket.on("call:disconnected", ({ to })=>{
-        io.to(to).emit("call:disconnected",{to:to});
-      });
-      socket.on("video:toggle",({to,isVideoEnabled})=>{
-        io.to(to).emit("video:toggle",{to:to,isVideoEnabled:isVideoEnabled});
-      })
-      socket.on("audio:toggle", ({ to, isAudioEnabled }) => {
-        io.to(to).emit("audio:toggle", { to: to, isAudioEnabled: isAudioEnabled });
-    });
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, roomID } = data;
+    console.log("Email", email, "RoomID", roomID)
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(roomID).emit("user:joined", { email, id: socket.id });
+    socket.join(roomID);
+    io.to(socket.id).emit("room:join", data);
+  });
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+  socket.on("call:disconnected", ({ to }) => {
+    io.to(to).emit("call:disconnected", { to: to });
+  });
+  socket.on("video:toggle", ({ to, isVideoEnabled }) => {
+    io.to(to).emit("video:toggle", { to: to, isVideoEnabled: isVideoEnabled });
+  })
+  socket.on("audio:toggle", ({ to, isAudioEnabled }) => {
+    io.to(to).emit("audio:toggle", { to: to, isAudioEnabled: isAudioEnabled });
+  });
 })
 
 const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
-    console.log(`Server connected on ${PORT}`);
+  console.log(`Server connected on ${PORT}`);
 });
