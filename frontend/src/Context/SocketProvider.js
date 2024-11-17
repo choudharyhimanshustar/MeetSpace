96a6ffa41c56914d43e6c6f19938e86ab4e5e1d7
AbhishekJha-45 <krjha36555@gmail.com>
2024-11-17 21:45:43 +0530

    Update socket URL to remove port number

--- frontend/src/Context/SocketProvider.js
+++ frontend/src/Context/SocketProvider.js
@@ -3,23 +3,14 @@ import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
-const SOCKET_URL = "https://meetspace-zt28.onrender.com:2001";
 export const useSocket = () => {
   const socket = useContext(SocketContext);
   return socket;
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => {
-    if (!SOCKET_URL) {
-      console.error("Socket.IO connection failed");
-      return;
-    }
-    io(SOCKET_URL);
-  }, []);
-  // if (!socket) {
-  //   return <div>Socket.IO connection failed</div>;
-  // }
+  const socket = useMemo(() => io("https://meetspace-zt28.onrender.com"), []);
+
   return (
     <SocketContext.Provider value={socket}>
       {props.children}
